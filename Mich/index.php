d743802c77f9c83f556a456ff9ef694fc1e421ce
DuongRet <66632174+DuongRet@users.noreply.github.com>
2020-06-09 23:24:51 +0700

    duong

--- Mich/index.php
+++ Mich/index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Githut</title>
 </head>
 <body>
-    
+    <h1>Hello all in chab hit<h1>
 </body>
 </html>
