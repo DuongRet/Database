db8d3e171961775dc2e026b55951dc8252f94c1f
Tolaken123 <66671640+Tolaken123@users.noreply.github.com>
2020-06-09 23:29:17 +0700

    omg

--- Mich/index.php
+++ Mich/index.php
@@ -6,13 +6,10 @@
     <title>Githut</title>
 </head>
 <body>
-<<<<<<< HEAD
     <h1>Hello all in chab hit<h1>
-=======
-    <h1>Hello world</h1>
     <ul>
-    <li>hi</li>
+    <li>omg</li>
+    
     </ul>
->>>>>>> c5d045a3425aa3a5dd315beb0e9c321f80d12d43
 </body>
 </html>
