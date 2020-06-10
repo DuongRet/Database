4e8ffbf3fe78eb580de3f786dc7cdc2dbda909ee
NeuyMichSR <56514563+NeuyMichSR@users.noreply.github.com>
2020-06-10 09:30:57 +0700

    mich update

--- Duong/makewebsite/body/body.php
+++ Duong/makewebsite/body/body.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
@@ -10,53 +11,63 @@
   <link href="css/custom.css" rel="stylesheet">
   <style>
     .affix {
-    top: 0;
-    width: 100%;
-    z-index: 9999 !important;
-  }
+      top: 0;
+      width: 100%;
+      z-index: 9999 !important;
+    }
 
-  .affix + .container-fluid {
-    padding-top: 70px;
-  }
+    .affix+.container-fluid {
+      padding-top: 70px;
+    }
   </style>
 </head>
-<body>
-  
-<div class="container-fluid">
-    <div style="background-color: lightyellow;" id="header1"> 
-   <h1 class="text-center">&nbsp;School</h1>
-   </div>
 
-<nav class="navbar" width="100%" data-spy="affix" data-offset-top="1">
-  <ul class="nav nav-tabs">
-    <li><a class="active" data-toggle="pill" href="#">Videos</a></li>
-    <li><a data-toggle="pill" href="#">Student List</a></li>
-    <li><a data-toggle="pill" href="#">Teacher List</a></li>
-    <li><a data-toggle="pill" href="#">Book list</a></li>
-    <li><a data-toggle="pill" href="#">Calender</a></li>
-    <li><a data-toggle="pill" href="#">Schedule for Student</a></li>
-    <li><a data-toggle="pill" href="#">Schedule for Teacher</a></li>
-    <li><a data-toggle="pill" href="#">Form new student</a></li>
-    <li><a data-toggle="pill" href="#">Traveling</a></li>
-    <li><a data-toggle="pill" href="#">Traveling</a></li>
-    <li><a data-toggle="pill" href="#">Traveling</a></li>
-    <li><a data-toggle="pill" href="#">Traveling</a></li>
-    <li><a data-toggle="pill" href="#">Traveling</a></li>
-  </ul>
-</nav>
-<div>
-  <table class="table" border="1">
-    <thead>
-      <tr>
-        <th>N<sup>o</sup></th>
-        <th>Name</th>
-        <th>Sex</th>
-        <th>Date of birth</th>
-        <th>Class</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
+<body>
+
+  <div class="container-fluid">
+    <div style="background-color: lightyellow;" id="header1">
+      <h1 class="text-center">&nbsp;School</h1>
+    </div>
+
+    <nav class="navbar" width="100%" data-spy="affix" data-offset-top="1">
+      <ul class="nav nav-tabs">
+        <li><a class="active" data-toggle="pill" href="#">Videos</a></li>
+        <li><a data-toggle="pill" href="#">Student List</a></li>
+        <li><a data-toggle="pill" href="#">Teacher List</a></li>
+        <li><a data-toggle="pill" href="#">Book list</a></li>
+        <li><a data-toggle="pill" href="#">Calender</a></li>
+        <li><a data-toggle="pill" href="#">Schedule for Student</a></li>
+        <li><a data-toggle="pill" href="#">Schedule for Teacher</a></li>
+        <li><a data-toggle="pill" href="#">Form new student</a></li>
+        <li><a data-toggle="pill" href="#">Traveling</a></li>
+        <li><a data-toggle="pill" href="#">Traveling</a></li>
+        <li><a data-toggle="pill" href="#">Traveling</a></li>
+        <li><a data-toggle="pill" href="#">Traveling</a></li>
+        <li><a data-toggle="pill" href="#">Traveling</a></li>
+      </ul>
+    </nav>
+    <div>
+      <table class="table" border="1">
+        <thead>
+          <tr>
+            <th>N<sup>o</sup></th>
+            <th>Name</th>
+            <th>Sex</th>
+            <th>Date of birth</th>
+            <th>Class</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php for($i = 1; $i <=10; $i++){ ?>
+            <tr>
+              <td><?= $i ?></td>
+              <td>Doung</td>
+              <td>Female</td>
+              <td>03/04/2000</td>
+              <td>PHP Tutorial</td>
+            </tr>
+          <?php } ?>
+          <!-- <tr>
        <td></td>
        <td></td>
        <td></td>
@@ -76,20 +87,14 @@
        <td></td>
        <td></td>
        <td></td>
-      </tr>
-      <tr>
-       <td></td>
-       <td></td>
-       <td></td>
-       <td></td>
-       <td></td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-</div>
+      </tr> -->
+        </tbody>
+      </table>
+    </div>
+  </div>
 
 </body>
+
 </html>
 
 
