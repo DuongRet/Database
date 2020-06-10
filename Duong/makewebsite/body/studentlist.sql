c67dd763dbf18270a8f0fecef071b032a87a2030
NeuyMichSR <56514563+NeuyMichSR@users.noreply.github.com>
2020-06-10 16:48:25 +0700

    mich

--- Duong/makewebsite/body/studentlist.sql
+++ Duong/makewebsite/body/studentlist.sql
@@ -11,7 +11,12 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Data exporting was unselected.
+-- Dumping data for table studentlist.studentlist: 2 rows
+/*!40000 ALTER TABLE `studentlist` DISABLE KEYS */;
+REPLACE INTO `studentlist` (`id`, `name`, `sex`, `date`, `class`) VALUES
+	(1, 'duong', 'female', '2020-06-17', 'program'),
+	(2, 'dem', 'male', '2000-06-16', 'Networking');
+/*!40000 ALTER TABLE `studentlist` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
