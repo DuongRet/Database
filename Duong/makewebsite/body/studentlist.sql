0fc60de2c0b9e0dc7bcb7b15e962bd363100b7fc
NeuyMichSR <56514563+NeuyMichSR@users.noreply.github.com>
2020-06-10 16:53:42 +0700

    mich

--- Duong/makewebsite/body/studentlist.sql
+++ Duong/makewebsite/body/studentlist.sql
@@ -1,23 +1,52 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.7.24 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             10.3.0.5771
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 10, 2020 at 09:53 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping data for table studentlist.studentlist: 2 rows
-/*!40000 ALTER TABLE `studentlist` DISABLE KEYS */;
-REPLACE INTO `studentlist` (`id`, `name`, `sex`, `date`, `class`) VALUES
-	(1, 'duong', 'female', '2020-06-17', 'program'),
-	(2, 'dem', 'male', '2000-06-16', 'Networking');
-/*!40000 ALTER TABLE `studentlist` ENABLE KEYS */;
+--
+-- Database: `studentlist`
+--
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentlist`
+--
+
+DROP TABLE IF EXISTS `studentlist`;
+CREATE TABLE IF NOT EXISTS `studentlist` (
+  `id` int(30) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `sex` varchar(250) NOT NULL,
+  `date` date NOT NULL,
+  `class` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `studentlist`
+--
+
+INSERT INTO `studentlist` (`id`, `name`, `sex`, `date`, `class`) VALUES
+(1, 'duong', 'female', '2020-06-17', 'program'),
+(2, 'dem', 'male', '2000-06-16', 'Networking');
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
